f723aa05e8aa4f864ba61ef9e107661215996c05
jsubarte <jsubarte@users.noreply.github.com>
2024-11-06 23:49:46 -0500

    Se mejoro la configuracion de base de datos

--- database/config.ts
+++ database/config.ts
@@ -1,12 +1,13 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize, Dialect } from 'sequelize'
+import 'dotenv/config'
 
 const dbConnection: Sequelize = new Sequelize(
     {
-        database: process.env.DB_NAME,
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        host: process.env.DB_HOST, 
-        dialect: 'postgres',
+        database: process.env.DB_NAME || '',
+        username: process.env.DB_USER || '',
+        password: process.env.DB_PASS || '',
+        host: process.env.DB_HOST || '', 
+        dialect: (process.env.DB_DIAL || 'postgres') as Dialect,
         port: Number(process.env.DB_PORT),
         logging: false
     }
@@ -15,7 +16,7 @@ const dbConnection: Sequelize = new Sequelize(
 const testConnection = async () => {
     try {
         await dbConnection.authenticate()
-        console.log(`Conexion a la base de datos establecida con exito`)
+        console.log(`Conexion a la base de datos ${ process.env.DB_NAME } establecida con exito`)
     } catch (error) {
         console.log(`Imposible conectar con la base de datos. Error: ${error}`)
     }
